ccf5b39752fbdf230e2f3d5f497c58f33aad73c5
lineng <lineng01213@163.com>
2023-07-06 00:37:03 +0800

    acapp login

---
+++ game/views/setting/acwing/acapp/apply_code.py
@@ -0,0 +1,29 @@
+from django.http import JsonResponse
+from urllib.parse import quote
+from random import randint
+from django.core.cache import cache
+
+
+def get_state():
+    res = ""
+    for i in range(8):
+        res += str(randint(0, 9))
+    return res
+
+
+def apply_code(request):
+    appid = "5595";
+    redirect_uri = quote("http://8.130.66.193:8000/setting/acwing/acapp/receive_code")
+    scope = "userinfo"
+    state = get_state()
+
+    cache.set(state, True, 7200)  # 有效期==2h
+
+    return JsonResponse({
+        'result': "success",
+        'appid': appid,
+        'redirect_uri': redirect_uri,
+        'scope': scope,
+        'state': state,
+    })
+
--- static/js/dist/game.js
+++ static/js/dist/game.js
@@ -557,8 +557,12 @@ class Setting {
     }
 
     start() {
-        this.getinfo();
-        this.add_listening_events();
+        if (this.platform === "ACAPP") {
+            this.getinfo_acapp();
+        } else {
+            this.getinfo_web();
+            this.add_listening_events();
+        }
     }
 
     add_listening_events() {
@@ -602,7 +606,7 @@ class Setting {
             success: function(resp){
                 console.log(resp);
                 if(resp.result === "success") {
-                   window.location.replace(resp.apply_code_url);
+                    window.location.replace(resp.apply_code_url);
                 }
             }
         });
@@ -683,7 +687,36 @@ class Setting {
         this.$login.show();
     }
 
-    getinfo() {
+    acapp_login(appid, redirect_uri, scope, state) {
+        let outer = this;
+
+        this.root.AcWingOS.api.oauth2.authorize(appid, redirect_uri, scope, state, function(resp) {
+            console.log("called from acapp_login function");
+            console.log(resp);
+            if (resp.result === "success") {
+                outer.username = resp.username;
+                outer.photo = resp.photo;
+                outer.hide();
+                outer.root.menu.show();
+            }
+        });
+    }
+
+    getinfo_acapp() {
+        let outer = this;
+
+        $.ajax({
+            url: "http://8.130.66.193:8000/settings/acwing/acapp/apply_code/",
+            type: "GET",
+            success: function(resp) {
+                if (resp.result === "success") {
+                    outer.acapp_login(resp.appid, resp.redirect_uri, resp.scope, resp.state);
+                }
+            }
+        });
+    }
+
+    getinfo_web() {
         let outer = this;
         $.ajax({
             url:"http://8.130.66.193:8000/setting/getinfo/",
